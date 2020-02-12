a5aec81b092fbe72cdde0463fd934370f9eca24f
swpolgla <swpolgla@Stevens-MacBook-Pro.local>
2020-02-12 14:22:33 -0500

    Simple bot functionality added. Can respond to /start with a message.

--- src/minecraft-bot.rb
+++ src/minecraft-bot.rb
@@ -1,22 +1,43 @@
 # This bot manages a specified Minecraft server hosted on DigitalOcean.
 require 'discordrb'
 
+TOKEN_NOT_FOUND = "token=INSERT DISCORD BOT TOKEN HERE"
+APPID_NOT_FOUND = "appID=INSERT APPLICATION ID HERE"
+
 # Generates empty token/application ID files if they do not already exist
 if !File.exist?("token.txt") or !File.exist?("appID.txt")
-    File.write("token.txt", "appID=INSERT DISCORD BOT TOKEN HERE")
-    File.write("appID.txt", "appID=INSERT APPLICATION ID HERE")
+    File.write("token.txt", TOKEN_NOT_FOUND)
+    File.write("appID.txt", APPID_NOT_FOUND)
     puts("Files have been created to store your bot's unique token and application ID. \nPlease place the necessary information into those files.")
-    return
-
+end
 
 # Reads your specific application ID/token from their individual files
-token = File.read("/token.txt")
-appID = File.read("/appID.txt")
+token = File.read("token.txt")
+if token == TOKEN_NOT_FOUND
+    puts("Please insert your bot's unique token into token.txt before continuing.")
+    puts("This token can be found within your Discord Dev Portal.")
+    return
+end
+token = token[6..token.length]
+
+appID = File.read("appID.txt")
+if token == TOKEN_NOT_FOUND
+    puts("Please insert your bot's Application ID into appid.txt before continuing.")
+    puts("This token can be found within your Discord Dev Portal.")
+    return
+end
+appID = appID[6..appID.length]
+
+
 
 # Creates the bot with a token/application ID generated from your Discord
 # Developer Portal.
-# bot = Discordrb::Bot.new token: token
+bot = Discordrb::Bot.new token: token
 
 # Prompt to invite the bot to your server if necessary
 puts "Invite the bot to your server:" # {bot.invite_url}"
+
+bot.message(content: '/start') do |event|
+    event.respond 'Server Starting'
 end
+bot.run
